ae516145136e85b3fcc2a457157201b9bea500c0
Josh McIntyre <josh.r.mcintyre13@gmail.com>
2018-11-19 03:07:57 -0800

    remove contact form

--- contact.php
+++ contact.php
@@ -4,10 +4,10 @@ require('recaptcha-master/src/autoload.php');
 
 // configure
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = 'demo@domain.com';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <demo@domain.com>';
+$sendTo = 'josh.r.mcintyre13@gmail.com';
 
 // subject of the email
 $subject = 'New message from contact form';
