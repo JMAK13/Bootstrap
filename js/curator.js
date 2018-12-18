095573a0d65e8670e944cc90951cd64c8718edcf
Josh McIntyre <josh.r.mcintyre13@gmail.com>
2018-12-18 14:03:19 -0800

    Style Update v1

---
+++ js/curator.js
@@ -0,0 +1,6 @@
+// curator-feed
+(function(){
+    var i, e, d = document, s = "script";i = d.createElement("script");i.async = 1;
+    i.src = "https://cdn.curator.io/published/89f09c1a-2a4c-47b2-b1cd-d2d1ae55.js";
+    e = d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
+})();
---
+++ js/main.js
@@ -0,0 +1,11 @@
+var menu = document.querySelector('.navbar');
+		var wrapperMenu = document.querySelector('.wrapper-menu');
+		var navBar = document.querySelector('.navbar-collapse');
+		var menuBtn = document.querySelector('.navbar-toggler');
+
+		menuBtn.addEventListener('click', function(){
+		  if(navBar.className !== "navbar-collapse justify-content-center align-self-start collapsing"){
+		    wrapperMenu.classList.toggle('open');
+		    menu.classList.toggle('open2');
+		  }
+		})
