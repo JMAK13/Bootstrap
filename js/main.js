6b859caf16e3b94ac8f5bb8fe6313c2ef38a80b9
Josh McIntyre <josh.r.mcintyre13@gmail.com>
2018-12-20 04:46:36 -0800

    Landing Page Rework
    hamburger toggler is still screwed up after github desktop made me want to commit suicide

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 var menu = document.querySelector('.navbar');
-<<<<<<< HEAD
 var wrapperMenu = document.querySelector('.wrapper-menu');
 var navBar = document.querySelector('.navbar-collapse');
 var menuBtn = document.querySelector('.navbar-toggler');
@@ -10,15 +9,3 @@ menuBtn.addEventListener('click', function(){
     menu.classList.toggle('open2');
   }
 })
-=======
-		var wrapperMenu = document.querySelector('.wrapper-menu');
-		var navBar = document.querySelector('.navbar-collapse');
-		var menuBtn = document.querySelector('.navbar-toggler');
-
-		menuBtn.addEventListener('click', function(){
-		  if(navBar.className !== "navbar-collapse justify-content-center align-self-start collapsing"){
-		    wrapperMenu.classList.toggle('open');
-		    menu.classList.toggle('open2');
-		  }
-		})
->>>>>>> 095573a0d65e8670e944cc90951cd64c8718edcf
