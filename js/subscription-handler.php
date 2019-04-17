1e7ffc331d6477eec6a7507a738bd71c1335b187
Josh McIntyre <josh.r.mcintyre13@gmail.com>
2019-04-17 15:40:46 -0500

    Fix Email Sanitization
    Small bug where unique emails weren't being written to the .txt file; however, the new input validation method has been tested and works.

--- js/subscription-handler.php
+++ js/subscription-handler.php
@@ -1,13 +1,16 @@
 <?php
 
-	//Write to file
+	//PHP Script by Josh McIntyre
+	//Writes unique email form submissions from html page to .txt file
+	//then redirects the user back to the Home page after 3 seconds
 	$myfile = fopen("newfile.txt", "r+") or die("File does not exist!");	//opens file in update mode(read&write)
-	$tmp = $_POST['userEm'];
 	$email = $_POST['userEm'].PHP_EOL;			//PHP.EOL is newline (/n in C++)
-	while(!feof($myfile)) {
+	$found = false;
+	while(! feof($myfile)) {
   		$line = fgets($myfile);				//reads in each line of the .txt file
-		if($line === $tmp) fwrite($myfile, $email);	//sanitizes the input, only writing unique emails
+		if($line === $email) $found = true;		//checks for duplicate emails
   	}
+	if(! $found) fwrite($myfile, $email);			//sanitizes the input, only writing unique emails
 	fclose($myfile);
 
 	//Redirects back to Contact Page
