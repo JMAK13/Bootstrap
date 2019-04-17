4a42bf798452f72c11968d2c1a9a003994132fe4
Josh McIntyre <josh.r.mcintyre13@gmail.com>
2019-04-17 06:13:47 -0500

    Website is LIVE and DONE!
    url: http://www.rccsuits.com/index.html
    
    images are optimized, php form input sanitized, added 3sec redirect page and successful submission msg for Contact page, rearranged sections of About page to match cards of Home page, and a couple other things

--- js/subscription-handler.php
+++ js/subscription-handler.php
@@ -1,14 +1,20 @@
 <?php
 
 	//Write to file
-	$myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
-	$email = $_POST['userEm'].PHP_EOL;	//PHP.EOL is newline (/n in C++)
-	fwrite($myfile, $email);
+	$myfile = fopen("newfile.txt", "r+") or die("File does not exist!");	//opens file in update mode(read&write)
+	$tmp = $_POST['userEm'];
+	$email = $_POST['userEm'].PHP_EOL;			//PHP.EOL is newline (/n in C++)
+	while(!feof($myfile)) {
+  		$line = fgets($myfile);				//reads in each line of the .txt file
+		if($line === $tmp) fwrite($myfile, $email);	//sanitizes the input, only writing unique emails
+  	}
 	fclose($myfile);
 
 	//Redirects back to Contact Page
 	if ( isset( $_POST['submit'] ) ) {
-		header('Location: http://localhost/Bootstrap/Contact.html');
+		echo "<h2>You have successfully subscribed to the Team Syrus Newsletter!</h2>";
+		echo "<h3>Redirecting you back to the Home page in 3 seconds...</h3>";
+		header('Refresh: 3; URL=http://www.rccsuits.com/index.html');
 	}
 	exit;
 ?>
